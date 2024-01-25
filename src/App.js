a0532417355072da099e2fd571de2c2a164bd552
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-25 22:10:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [documentTitle, setDocumentTitle] = useState("");
+
+  useEffect(() => {
+    document.title = documentTitle;
+  }, [documentTitle]);
+
+  const handleTitleChange = (e) => {
+    setDocumentTitle(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <label htmlFor="title-input">
+        Type in the input and look at the browser tab!
+      </label>
+      <input
+        id="title-input"
+        type="text"
+        maxLength={25}
+        onChange={handleTitleChange}
+      />
+    </div>
+  );
+};
+
+export default App;
